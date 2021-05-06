0ea079cb97e71b5f203dec92581d5fc11816b8da
Andrey Kartashov <gorilych@gmail.com>
2021-05-06 13:03:35 +0700

    upgrade bazelisk(1.8.1)

--- scripts/install.bazelisk.sh
+++ scripts/install.bazelisk.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-version=v1.7.5
+version=v1.8.1
 dist=https://github.com/bazelbuild/bazelisk/releases/download/${version}/bazelisk-darwin-amd64
 file=/usr/local/bin/bazel
 
-wget -O ${file} -c ${dist}
+wget -O ${file} ${dist}
 
 chmod +x ${file}
